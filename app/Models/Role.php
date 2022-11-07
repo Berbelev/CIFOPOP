0feb486f9db01f7a6d5b2cb0c6f2385e8503a3a7
Eva Berbel <berbelev@gmail.com>
2022-11-07 06:40:12 +0100

    v.0.11.1 class Role Not found

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,17 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+
+
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model{
+class Role extends Model
+{
+    protected $fillable = ['role'];
 
-    use HasFactory;
-
-    protected $fillable=['role', 'descripcion'];
-
-    // recupera los usuarios con este rol
+    // recupera los usuarios con este Rol
     public function users(){
-        return $this->belongsToMany('App\Models\User');
+        return $this->belongsToMany('User');
     }
 }
